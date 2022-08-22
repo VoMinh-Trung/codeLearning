23e79b05ce39355977831d0d50320b094e71c046
VoMinhTrung <97ga2000@gmail.com>
2022-08-22 13:03:29 +0700

    update

---
+++ CTDL-GT2/CTDL-GT2/singleLinked.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+struct node
+{
+	int data;//data 
+	struct node* pNext;//pointer
+};
+typedef struct node NODE;//replay name "struct node" to only one name "NODE"
+
+void main() 
+{
+
+}
