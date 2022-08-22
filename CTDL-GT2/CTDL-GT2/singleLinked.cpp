81fd97cf6f9b42e04016fb3909c495f115bb2551
VoMinhTrung <97ga2000@gmail.com>
2022-08-22 16:54:09 +0700

    update

--- CTDL-GT2/CTDL-GT2/singleLinked.cpp
+++ CTDL-GT2/CTDL-GT2/singleLinked.cpp
@@ -8,6 +8,28 @@ struct node
 };
 typedef struct node NODE;//replay name "struct node" to only one name "NODE"
 
+
+//declare struction of single list
+struct list
+{
+	NODE* pHead;//first node
+	NODE* pTail;//end node
+};
+typedef struct list LIST;//replay name "struct list" to only one name "LIST"
+
+//initialization single list
+void khoiTao(LIST &l)
+{
+	l.pHead = NULL;
+	l.pTail = NULL;
+}
+
+//function initialization a node
+NODE* khoiTaoNODE()
+{
+
+}
+
 void main() 
 {
 
