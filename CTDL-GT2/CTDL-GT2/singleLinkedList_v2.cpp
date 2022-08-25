69dc73b1fa4353a73775c1b94a005134fc16cd4e
VoMinhTrung <97ga2000@gmail.com>
2022-08-25 16:49:58 +0700

    update 2508

--- CTDL-GT2/CTDL-GT2/singleLinkedList_v2.cpp
+++ CTDL-GT2/CTDL-GT2/singleLinkedList_v2.cpp
@@ -27,10 +27,84 @@ NODE *initNODE(int x)
 	{
 		return NULL;
 	}
+	p->data = x;
+	p->pNext = NULL;
+	return p;
 }
 
+void addHEAD(LIST &l, NODE *p)
+{
+	if (l.pHead == NULL)
+	{
+		l.pHead = l.pTail = NULL;
+	}
+	else
+	{
+		p->pNext = l.pHead;
+		l.pHead = p;
+	}
+}
+
+void addTAIL(LIST &l, NODE *p)
+{
+	if (l.pHead == NULL)
+	{
+		l.pHead = l.pTail = NULL;
+	}
+	else
+	{
+		l.pTail->pNext = p;
+		l.pTail = p;
+	}
+}
+
+//export list
+void exportList(LIST l)
+{
+	for (NODE* k = l.pHead; k != NULL; k = k->pNext)
+	{
+		printf_s("%d  ", k->data);
+	}
+}
+
+void MENU()
+{
+	int option;
+	bool breakWhile = 1;
+	while (breakWhile)
+	{
+		printf_s("\n\n--------- MENU ---------");
+		printf_s("\n\n--------- 0.EXIT ---------");
+		printf_s("\n\n--------- 1.INPUT VALUE ---------");
+		printf_s("\n\n--------- 2.EPORT ---------");
+		printf_s("\n\n--------- END ---------");
+
+		printf_s("\nInput your select: ");
+		scanf_s("%d", &option);
+
+		switch (option)
+		{
+			case 1:
+			{
+				printf_s("\nNhap gia tri so nguyen");
+				break;
+			}
+			case 2:
+			{
+				printf_s("\nXuat mang lien ket");
+				break;
+			}
+			case 0:
+			{
+				printf_s("\nEXIT");
+				breakWhile = 0;
+				break;
+			}
+		}
+	}
+}
 
 void main() 
 {
-
+	MENU();
 }
